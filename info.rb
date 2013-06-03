5c1f8ea6b3e052008722250c5e7bdbfd159fffce
Eric Dykstra <eric@ericdykstra.me>
2013-06-02 22:04:42 -0700

    init commit

---
+++ info.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+  content_type :json
+  hash = { "name" => "eric dykstra",
+    "email" => "eric@ericdykstra.me",
+    "phone" => "+18553957872",
+    "github" => "http=>//github.com/ericdykstra",
+    "twitter" => "@eric_dykstra",
+    "linkedin" => "http://linkedin.com/in/ericjdykstra" }
+  hash.to_json
+end
