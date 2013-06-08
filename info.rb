9580dba51da23e1906c4427daf9816876f4ef80c
Eric Dykstra <eric@ericdykstra.me>
2013-06-07 17:41:18 -0700

    Now you can go to /email to get email, etc...

--- info.rb
+++ info.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
 require 'json'
 
+hash = { "name" => "eric dykstra",
+  "email" => "eric@ericdykstra.me",
+  "phone" => "+18553957872",
+  "github" => "http://github.com/ericdykstra",
+  "portfolio" => "http://ericdykstra.me",
+  "twitter" => "@eric_dykstra",
+  "linkedin" => "http://linkedin.com/in/ericjdykstra" }
+
 get '/' do
   content_type :json
-  hash = { "name" => "eric dykstra",
-    "email" => "eric@ericdykstra.me",
-    "phone" => "+18553957872",
-    "github" => "http://github.com/ericdykstra",
-    "twitter" => "@eric_dykstra",
-    "linkedin" => "http://linkedin.com/in/ericjdykstra" }
   hash.to_json
+end
+
+get '/:id' do
+  hash[params[:id]]
 end
